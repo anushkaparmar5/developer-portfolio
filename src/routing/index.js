f8d674f2ed8734bcb807bc154e173a02c43a9876
Bansi Parmar <102733409+anushkaparmar5@users.noreply.github.com>
2024-01-24 16:32:58 +0530

    Update index.js

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://anushkaparmar5.github.io/developer-portfolio';
+const BASE_URL = 'https://anushkaparmar5.github.io';
 const URLS = {
     Home: `${BASE_URL}/`,
     About: `${BASE_URL}/#about`,
