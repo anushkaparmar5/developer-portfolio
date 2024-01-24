3744625c66f74b7cf8b5176cac7258020e3f6a1c
Bansi Parmar <102733409+anushkaparmar5@users.noreply.github.com>
2024-01-24 17:10:58 +0530

    Update index.js

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,13 +1,11 @@
-// const BASE_URL = 'https://anushkaparmar5.github.io/developer-portfolio/';
-const BASE_URL = '/';
 const URLS = {
-    Home: `${BASE_URL}`,
-    About: `${BASE_URL}#about`,
-    Contacts: `${BASE_URL}#contacts`,
-    Education: `${BASE_URL}#education`,
-    Skills: `${BASE_URL}#skills`,
-    Services: `${BASE_URL}#services`,
-    Projects: `${BASE_URL}#projects`,
+    Home: '',
+    About: 'about',
+    Contacts: 'contacts',
+    Education: 'education',
+    Skills: 'skills',
+    Services: 'services',
+    Projects: 'projects',
 };
 
 export default URLS;
