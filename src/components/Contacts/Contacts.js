c066d0f8a126bf87fdd1ff3eca33768158ef2870
Bansi Pamrar <anushkaparmar512@gmail.com>
2024-01-25 10:37:44 +0530

    fixed issue

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -170,7 +170,7 @@ function Contacts() {
                                     Name
                                 </label>
                                 <input
-                                    placeholder='John Doe'
+                                    placeholder='Enter you full name'
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
                                     type='text'
@@ -186,7 +186,7 @@ function Contacts() {
                                     Email
                                 </label>
                                 <input
-                                    placeholder='John@doe.com'
+                                    placeholder='Enter your email'
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                     type='email'
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,7 @@
-import resume from '../assets/pdf/BansiParmar_ReactJsDevloper.pdf'
-
 export const headerData = {
     name: 'Bansi Parmar',
     title: "ReactJs Developer",
     desciption: "Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
     image: 'https://avatars.githubusercontent.com/u/102733409?v=4',
-    resumePdf: resume
+    resumePdf: "https://drive.google.com/drive/folders/1NpI7f4Nbm5NwpHU0xb1y1pM9R6U5hr2q"
 }
